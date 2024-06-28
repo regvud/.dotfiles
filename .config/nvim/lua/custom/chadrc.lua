ae3cded8c1c4360b5fd81ceeafaceb4e0dcd3a21
regvud <silveron.regvud@gmail.com>
2024-06-28 17:48:53 +0300

    nvim remote repo fix

---
+++ .config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,14 @@
+---@type ChadrcConfig
+local M = {}
+
+
+M.ui = {
+  theme = "everforest_light",
+  tabufline = {
+    enabled = false
+  },
+  telescope = { style = "bordered" },
+}
+
+M.plugins = "custom.plugins"
+return M
