11bfa0e264d3df9049bd76b19958150871c8c53e
regvud <silveron.regvud@gmail.com>
2024-07-21 12:39:41 +0300

    mappings for git

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -13,9 +13,15 @@ map("n", "<leader>ds", "<NOP>")
 
 map("n", "<leader>fz", "<NOP>")
 map("n", "<leader>ff", "<NOP>")
+map("n", "<leader>cc", "<NOP>")
+map("n", "<leader>gt", "<NOP>")
 
 --general
 map({ "n", "i", "v" }, "<C-c>", "<ESC>")
+
+--indentation
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
 --centering
 map("n", "<C-u>", "<C-u>zz")
@@ -28,8 +34,9 @@ map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 map("x", "p", [["_dP]])
 
 --Telescope
-map("n", "<leader>fd", "<cmd> Telescope find_files <cr>")
-map("n", "<leader>/", "<cmd> Telescope current_buffer_fuzzy_find <cr>")
+map("n", "<leader>fd", "<cmd> Telescope find_files <cr>", { desc = "telescope find directory" })
+map("n", "<leader>/", "<cmd> Telescope current_buffer_fuzzy_find <cr>", { desc = "search" })
+map("n", "<leader>fg", "<cmd> Telescope git_files <cr>", { desc = "telescope find git files" })
 
 --lsp
 map("n", "K", function()
@@ -43,3 +50,7 @@ end, { desc = "Diagnostics" })
 map("n", "<leader>lf", function()
   vim.diagnostic.setloclist()
 end, { desc = "Error list" })
+
+--git
+map("n", "<leader>gs", "<cmd> Telescope git_status <cr>", { desc = "git status" })
+map("n", "<leader>gc", "<cmd> Telescope git_commits <cr>", { desc = "git commits" })
